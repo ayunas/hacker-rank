e30ae2e65e30b56f79873a2146970e9b2d367c8b
Amir Yunas <ayunas@gmail.com>
2020-04-04 05:41:18 -0400

    used a set to determine a cycle.

--- happy_number.py
+++ happy_number.py
@@ -10,37 +10,55 @@
 # 62 + 82 = 100
 # 12 + 02 + 02 = 1
 
-from functools import reduce
-
-def happy_numbers(num):
-    arr = [int(n) for n in str(num)]
-    # summed = reduce(lambda s,n: s+n**2,arr)
-    summed = 0
-    for n in arr:
-        summed += n**2
-    print('summed', summed)
-    count = 0
-
-    while summed != 1:
-
-        arr = [int(n) for n in str(summed)]
-        summed = 0
-        for n in arr:
-            summed += n**2
-        count += 1
-        if count > 100000:
-            return False
+def happy_number(num:int) -> bool:
+    visited = set()
     
-    return True
+    while num != 1 and num not in visited:
+        visited.add(num)
+        print(visited)
+        arr = [int(n_str) for n_str in str(num)]
+        print(arr)
+        squares = map(lambda n: int(n)**2,arr)
+        num = sum(squares)
+        print(num)
+        
+    
+    return True if num == 1 else False
+    
 
-x = happy_numbers(23)
+x = happy_number(19)
 print(x)
 
+# from functools import reduce
+
+# def happy_numbers(num):
+#     arr = [int(n) for n in str(num)]
+#     # summed = reduce(lambda s,n: s+n**2,arr)
+#     summed = 0
+#     for n in arr:
+#         summed += n**2
+#     print('summed', summed)
+#     count = 0
+
+#     while summed != 1:
+
+#         arr = [int(n) for n in str(summed)]
+#         summed = 0
+#         for n in arr:
+#             summed += n**2
+#         count += 1
+#         if count > 100000:
+#             return False
+    
+#     return True
 
 
-def isHappy(self, n: int) -> bool:
+def isHappy(n: int) -> bool:
         visited = set()
         while n != 1 and not n in visited:
             visited.add(n)
             n = sum(map(lambda x:int(x)**2, str(n)))
         return not n in visited
+
+z = isHappy(238)
+print(z)
